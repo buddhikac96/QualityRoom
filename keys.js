05bb61033249bb6c7bbd15c547ee83b855c8cd1f
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-02-26 12:37:25 +0530

    keys file added

---
+++ keys.js
@@ -0,0 +1,25 @@
+var keys = {
+    options: {
+        host: 'mqtt://tailor.cloudmqtt.com',
+        port: 14183,
+        username: 'ileliujf',
+        password: 'or_W8gVd5_XD'
+    },
+
+    token: 'D41nD_HM7sQ7Bj2Ze4K0D64c0EvMoyFcvfPQPGJWxaF86BKPDC8OYsogDKEWz32ifSIHtqK0LsTQYU1hY5xwIA==',
+
+    db: {
+        host: 'remotemysql.com',
+        port: '3306',
+        database: 'H0i8XSbjI0',
+        username: 'H0i8XSbjI0',
+        password: 'zLYsD4LQYL',
+    },
+
+    twilio: {
+        accountSid: 'ACdce6e355969021eedd82046da5ec07a7',
+        authToken: '013f4d20f8f8124d3e0f51bf79556de8'
+    }
+}
+
+module.exports = keys;

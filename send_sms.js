7416f01e3f72abfa372840f899f7e603a3bbabf2
Buddhika Chathuranga <vmbckscc456@gmail.com>
2020-03-08 12:01:03 +0530

    sms sender changed

--- send_sms.js
+++ send_sms.js
@@ -7,9 +7,15 @@ const authToken = keys.twilio.authToken;
 const client = require('twilio')(accountSid, authToken);
 
 
-function sendSms(alertMessage, sensor, value) {
+var allowSendSMSTemp = true;
+var allowSendSMSHum = true;
+var allowSendSMSSound = true;
+var allowSendSMSAir = true;
 
-    var msg = createMessage(alertMessage, sensor, value);
+
+function sendSms(alertMessage, sensor, value, map) {
+
+    var msg = createMessage(alertMessage, sensor, value, map);
 
     client.messages
         .create({
@@ -22,25 +28,42 @@ function sendSms(alertMessage, sensor, value) {
 
 function validateSms(map){
 
-    if(map.temp > 35){
-        sendSms("High Temperature", "Temperature", map.temp);
+    if(map.temp >= 35 && allowSendSMSTemp){
+        sendSms("High Temperature", "Temperature", map.temp, map);
+        allowSendSMSTemp = false;
+    }else if(map.temp < 35){
+        allowSendSMSTemp = true;
     }
 
-    if(map.hum > 60){
-        sendSms("High Humidity", "Humidity", map.hum);
+    if(map.hum >= 65 && allowSendSMSHum){
+        sendSms("High Humidity", "Humidity", map.hum, map);
+        allowSendSMSHum = false;
+    }else if(map.hum < 65){
+        allowSendSMSHum = true;
     }
 
-    if(map.sound > 100){
-        sendSms("Dangerous sound level", "Sound", map.sound);
+    if(map.sound >= 120 && allowSendSMSSound){
+        sendSms("Dangerous sound level", "Sound", map.sound, map);
+        allowSendSMSSound = false;
+    }else if(map.sound < 120){
+        allowSendSMSSound = true;
     }
 
-    if(map.air > 150){
-        sendSms("Bad air condition", "Air Quality", map.air);
+    if(map.air >= 150 && allowSendSMSAir){
+        sendSms("Bad air condition", "Air Quality", map.air, map);
+        allowSendSMSAir = false;
+    }else if(map.air < 150){
+        allowSendSMSAir = true;
     }
 }
 
-function createMessage(alertMessage, sensor, value){
-    var msg = "Alert - " + alertMessage + " " + sensor + " - " + value;
+function createMessage(alertMessage, sensor, value, map){
+    var msg = "Alert  -  " + alertMessage + '\n';
+    msg += "Temperature  -  " + map.temp + String.fromCharCode(0x00B0) + "C\n";
+    msg += "Humidity  -  " + map.hum + " RH\n";
+    msg += "Sound  -  " + map.sound + " dB\n";
+    msg += "Air Quality  -  " + map.air + " ppm\n";
+
     return msg;
 }
 

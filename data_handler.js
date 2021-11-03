a558fde33a657bf0efba776cc2909096e4347f5a
Buddhika Chathuranga <vmbckscc456@gmail.com>
2021-11-04 00:52:37 +0530

    update query

--- data_handler.js
+++ data_handler.js
@@ -4,14 +4,14 @@ var writeData = (data) => {
     var sql = "INSERT INTO Sensor_Data(Temperature, Humidity, Sound, AirQuality) VALUES("+data.temp+", "+data.hum+", "+data.sound+", "+data.air+");";
     console.log(sql);
     
-    // try{
-    //     con.query(sql, (err, result) => {
-    //         if(err) throw err;
-    //         console.log(result);
-    //     });
-    // }catch(err){
-    //     throw err;
-    // }
+    try{
+        con.query(sql, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+        });
+    }catch(err){
+        throw err;
+    }
     
 }
 

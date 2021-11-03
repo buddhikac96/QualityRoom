44784bb2d251d26c6ff8ccff974a3c12aaca09cf
Buddhika Chathuranga <vmbckscc456@gmail.com>
2021-11-04 00:47:29 +0530

    sql query update

--- data_handler.js
+++ data_handler.js
@@ -1,7 +1,7 @@
 const con = require('./db');
 
 var writeData = (data) => {
-    var sql = "INSERT INTO Sensor_Data(Temperature, Humidity, Sound, AirQuality) VALUES("+data.temp+", "+data.hum+", "+data.sound+", "+data.air+")";
+    var sql = "INSERT INTO Sensor_Data(Temperature, Humidity, Sound, AirQuality) VALUES("+data.temp+", "+data.hum+", "+data.sound+", "+data.air+");";
     
     try{
         con.query(sql, (err, result) => {

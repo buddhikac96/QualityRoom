2f1e49958d1b6edf56348445722e4478d855b3c3
buchlk <buddhika.chathuranga@ifsworld.com>
2021-11-16 01:11:13 +0530

    update db and keys

--- data_handler.js
+++ data_handler.js
@@ -2,8 +2,7 @@ const con = require('./db');
 
 var writeData = (data) => {
     var sql = "INSERT INTO Sensor_Data(Temperature, Humidity, AirQuality) VALUES("+data.temp+", "+data.hum+", "+data.air+");";
-    console.log(sql);
-    
+    //console.log(sql);
     try{
         con.query(sql, (err, result) => {
             if(err) throw err;

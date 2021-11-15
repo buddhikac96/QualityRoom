d0f92e5600b7b0157efc33053d941757f2a913fd
buchlk <buddhika.chathuranga@ifsworld.com>
2021-11-16 00:58:47 +0530

    update conf

--- dbscript.sql
+++ dbscript.sql
@@ -5,11 +5,8 @@ create table Sensor_Data(
     AirQuality Float NULL 
 );
 
-INSERT INTO `Sensor_Data` (`Time_Stamp`, `Temperature`, `Humidity`, `AirQuality`) /
-VALUES (CURRENT_TIMESTAMP, '25.5', '76', '13'), (CURRENT_TIMESTAMP, '24', '78', '15');
+INSERT INTO `Sensor_Data` (`Time_Stamp`, `Temperature`, `Humidity`, `AirQuality`) VALUES (CURRENT_TIMESTAMP, '25.5', '76', '13'), (CURRENT_TIMESTAMP, '24', '78', '15');
 
-INSERT INTO `Sensor_Data` (`Time_Stamp`, `Temperature`, `Humidity`, `AirQuality`) /
-VALUES (CURRENT_TIMESTAMP, '23.5', '78', '15'), (CURRENT_TIMESTAMP, '24', '78', '15');
+INSERT INTO `Sensor_Data` (`Time_Stamp`, `Temperature`, `Humidity`, `AirQuality`) VALUES (CURRENT_TIMESTAMP, '23.5', '78', '15'), (CURRENT_TIMESTAMP, '24', '78', '15');
 
-INSERT INTO `Sensor_Data` (`Time_Stamp`, `Temperature`, `Humidity`, `AirQuality`) /
-VALUES (CURRENT_TIMESTAMP, '26.5', '74', '14'), (CURRENT_TIMESTAMP, '24', '78', '15');
+INSERT INTO `Sensor_Data` (`Time_Stamp`, `Temperature`, `Humidity`, `AirQuality`) VALUES (CURRENT_TIMESTAMP, '26.5', '74', '14'), (CURRENT_TIMESTAMP, '24', '78', '15');

0310e474a1bfab0133cd4f02826d358d929dcea0
buchlk <buddhika.chathuranga@ifsworld.com>
2021-11-16 21:38:02 +0530

    update database host

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const keys = require('./keys');
 
 var con = mysql.createConnection({
-    host: "8.218.233.170:3306",
+    host: "localhost:3306",
     user: "root",
     password: "my-secret-pw",
     database : "iot"
